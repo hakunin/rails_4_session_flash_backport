644eef8717d3d7de3f08d26a659da950791f1812
Michal Hantl <michal.hantl@gmail.com>
2015-01-28 17:19:12 +0100

    Lets just use warn to not break Rails 4.

--- lib/rails_4_session_flash_backport.rb
+++ lib/rails_4_session_flash_backport.rb
@@ -8,7 +8,7 @@ when 2
 when 3
   require 'rails_4_session_flash_backport/rails3/flash_hash'
 when 4
-  Rails.logger.warn "You're on Rails 4 so it's probably safe to remove the rails_4_session_flash_backport gem!"
+  warn "You're on Rails 4 so it's probably safe to remove the rails_4_session_flash_backport gem!"
 else
-  Rails.logger.warn "rails_4_session_flash_backport doesnt yet do anything on Rails #{Rails.version}"
+  warn "rails_4_session_flash_backport doesnt yet do anything on Rails #{Rails.version}"
 end
